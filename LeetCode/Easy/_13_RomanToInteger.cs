5f681a722d5d44d77a10328e882a9ad2857c59f6
manvit94 <manvit94@gmail.com>
2023-12-18 18:10:10 +0200

    Implemented 13 Roman to Integer solution

--- LeetCode/Easy/_13_RomanToInteger.cs
+++ LeetCode/Easy/_13_RomanToInteger.cs
@@ -15,6 +15,73 @@ public class RomanToInteger
 
     private static int ConvertToInt(string s)
     {
-        return 1;
+        var romanToInt = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int sum = 0;
+
+        var cArr = s.ToCharArray();
+        for (int i = 0; i < cArr.Length; i++)
+        {
+            if (i == cArr.Length - 1)
+            {
+                sum += romanToInt[cArr[i]];
+                break;
+            }
+
+            if (cArr[i] == 'I' && cArr[i + 1] == 'V')
+            {
+                sum += 4;
+                i++;
+                continue;
+            }
+            
+            if (cArr[i] == 'I' && cArr[i + 1] == 'X')
+            {
+                sum += 9;
+                i++;
+                continue;
+            }
+            
+            if (cArr[i] == 'X' && cArr[i + 1] == 'L')
+            {
+                sum += 40;
+                i++;
+                continue;
+            }
+            
+            if (cArr[i] == 'X' && cArr[i + 1] == 'C')
+            {
+                sum += 90;
+                i++;
+                continue;
+            }
+            
+            if (cArr[i] == 'C' && cArr[i + 1] == 'D')
+            {
+                sum += 400;
+                i++;
+                continue;
+            }
+            
+            if (cArr[i] == 'C' && cArr[i + 1] == 'M')
+            {
+                sum += 900;
+                i++;
+                continue;
+            }
+            
+            sum += romanToInt[cArr[i]];
+        }
+
+        return sum;
     }
 }
