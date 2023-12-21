c03989695f72b01618b0f971f8a990559a429e13
manvit94 <manvit94@gmail.com>
2023-12-21 14:21:29 +0200

    Solved 1672 RichestCustomerWealth

---
+++ LeetCode/Easy/_1672_RichestCustomerWealth.cs
@@ -0,0 +1,26 @@
+namespace LeetCode.Easy;
+
+public class RichestCustomerWealth
+{
+    [Fact]
+    public void RichestCustomerWealthTest()
+    {
+        foreach (var testData in GetTestData())
+        {
+            var result = MaximumWealth(testData.Accounts);
+            Assert.Equal(testData.Wealth, result);
+        }
+    }
+
+    private int MaximumWealth(int[][] accounts)
+    {
+        return accounts.Select(x => x.Sum()).OrderByDescending(x => x).FirstOrDefault();
+    }
+
+    private IEnumerable<(int[][] Accounts, int Wealth)> GetTestData()
+    {
+        yield return (new int[][] { [1, 2, 3], [3, 2, 1] }, 6);
+        yield return (new int[][] { [1, 5], [7, 3], [3, 5] }, 10);
+        yield return (new int[][] { [2, 8, 7], [7, 1, 3], [1, 9, 5] }, 17);
+    }
+}
